1c52318c097202e3464c9f97a4b3db5db1888b1c
danieljacometw <djacomee@tailorw.com>
2025-05-06 10:45:05 -0500

    Revert "Merge pull request #1854 from tailorw-sas/FixInvoiceNumberGeneration"
    This reverts commit 82bc2c942693737cd29551ce2761e486d0e6b977, reversing
    changes made to 9925e865519e1d9a1bea6498f74919f1b743e3e0.

--- invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/newCredit/CreateNewCreditCommandHandler.java
+++ invoicing/src/main/java/com/kynsoft/finamer/invoicing/application/command/manageInvoice/newCredit/CreateNewCreditCommandHandler.java
@@ -4,6 +4,7 @@ import com.kynsof.share.core.domain.RulesChecker;
 import com.kynsof.share.core.domain.bus.command.ICommandHandler;
 import com.kynsof.share.core.domain.exception.BusinessException;
 import com.kynsof.share.core.domain.exception.DomainErrorMessage;
+import com.kynsoft.finamer.invoicing.application.command.manageAttachment.create.CreateAttachmentCommand;
 import com.kynsoft.finamer.invoicing.domain.dto.*;
 import com.kynsoft.finamer.invoicing.domain.dtoEnum.EInvoiceStatus;
 import com.kynsoft.finamer.invoicing.domain.dtoEnum.EInvoiceType;
@@ -146,7 +147,7 @@ public class CreateNewCreditCommandHandler implements ICommandHandler<CreateNewC
 
         UUID attachmentDefault = null;
         for (ManageAttachmentDto attachmentDto : attachments) {
-            ManageAttachmentTypeDto attachmentType = attachmentDto.getType();
+            ManageAttachmentTypeDto attachmentType = attachmentDto.getAttachmentType();
             if (attachmentType.isAttachInvDefault()) {
                 attachmentDefault = attachmentDto.getId();
                 break;
@@ -187,10 +188,10 @@ public class CreateNewCreditCommandHandler implements ICommandHandler<CreateNewC
         }
     }
 
-    private List<ManageAttachmentDto> createAttachments(List<CreateNewCreditAttachmentRequest> attachmentCommands, String employeeId) {
+    private List<ManageAttachmentDto> createAttachments(List<CreateAttachmentCommand> attachmentCommands, String employeeId) {
         List<ManageAttachmentDto> attachments = new LinkedList<>();
         int cont = 0;
-        for (CreateNewCreditAttachmentRequest attachmentCommand : attachmentCommands) {
+        for (CreateAttachmentCommand attachmentCommand : attachmentCommands) {
             RulesChecker.checkRule(new ManageAttachmentFileNameNotNullRule(
                     attachmentCommand.getFile()
             ));
@@ -219,7 +220,7 @@ public class CreateNewCreditCommandHandler implements ICommandHandler<CreateNewC
 
             attachments.add(attachmentDto);
         }
-
+        //debe venir al menos un attachment de tipo attinvdefault
         if (cont == 0) {
             throw new BusinessException(
                     DomainErrorMessage.INVOICE_MUST_HAVE_ATTACHMENT_TYPE,
